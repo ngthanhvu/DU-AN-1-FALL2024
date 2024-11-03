2caac7207c8750295c5e4ed5f1bcc82389f07c5c
hoaily2005 <hoailxpk03594@gmail.com>
2024-11-03 22:27:34 +0700

    Update src

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import Home from '@/components/Home.vue';
+import Footer from '@/components/Footer.vue';
+
 </script>
 
 <template>
   <Navbar />
+  <Home />
+  <Footer />
+
 </template>
 
-<style>
+<style scoped>
 
 </style>
