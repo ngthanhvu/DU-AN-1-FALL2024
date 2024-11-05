a192a4df7b64520906e679a72d7ee9225544ea2d
hoaily2005 <hoailxpk03594@gmail.com>
2024-11-05 08:59:48 +0700

    npm icon fortawesome/ npm router / npm swiper

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import '@/assets/style.css'
 import '@/assets/main.css'
 import '@/assets/nomal.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// Import biểu tượng bạn cần từ Font Awesome
+import { faUser,faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app')
+
+// Thêm icon vào thư viện
+library.add(faUser,faHeart,faCartShopping,faPlus,faMinus);
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index.js'; 
+
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router); 
+app.mount('#app');
+
